89984cf6f746be3b06ebfe62da492b3176902240
mpatho <mpatho@psybergate.co.za>
2020-08-25 18:52:37 +0200

    initial commint

---
+++ src/test/java/com/psybergate/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psybergate.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
